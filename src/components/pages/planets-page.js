5b3d632cff2d2ac5b7f95a2903ecc9bde384313b
unrealisher <unrealmpa3@mail.ru>
2019-01-19 16:55:01 +0300

    Вынес star-db в отдельный репозиторий

---
+++ src/components/pages/planets-page.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import Row from "../row";
+import { PlanetList, PlanetDetails } from "../sw-components";
+
+export default class PlanetsPage extends React.Component {
+  state = {
+    selectedItem: null
+  };
+
+  onItemSelected = selectedItem => {
+    this.setState({
+      selectedItem
+    });
+  };
+
+  render() {
+    const { selectedItem } = this.state;
+    return (
+      <Row
+        left={<PlanetList onItemSelected={this.onItemSelected} />}
+        right={<PlanetDetails itemId={selectedItem} />}
+      />
+    );
+  }
+}
