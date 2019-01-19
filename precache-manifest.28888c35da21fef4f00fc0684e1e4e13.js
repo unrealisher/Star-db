e68e4a2ac9eb47fc4511d0bddd1140b4460aa62c
unrealisher <unrealmpa3@mail.ru>
2019-01-19 16:53:02 +0300

    Updates

---
+++ precache-manifest.28888c35da21fef4f00fc0684e1e4e13.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "91d934196997b5260173",
+    "url": "/Star-db/static/js/runtime~main.91d93419.js"
+  },
+  {
+    "revision": "4948227ae70cc24e615c",
+    "url": "/Star-db/static/js/main.4948227a.chunk.js"
+  },
+  {
+    "revision": "97735b829849d6ba5c2f",
+    "url": "/Star-db/static/js/1.97735b82.chunk.js"
+  },
+  {
+    "revision": "4948227ae70cc24e615c",
+    "url": "/Star-db/static/css/main.7d368ffe.chunk.css"
+  },
+  {
+    "revision": "8af20a3b8583199e8bf3a8e8df283744",
+    "url": "/Star-db/index.html"
+  }
+];
